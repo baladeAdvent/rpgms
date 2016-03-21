c182c65c1a0855777c24b9222d0d76842d599fae
David Perez <davidp@thecityof.net>
2016-03-21 15:23:55 -0500

    updating entities

--- webroot/www/src/AppBundle/Entity/Character.php
+++ webroot/www/src/AppBundle/Entity/Character.php
@@ -1,3 +1,16 @@
 <?php
+namespace AppBundle\Entity;
 
-?>
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *  @ORM\Entity
+ * @ORM\Table(name="character")
+ */
+Class Character
+{
+    protected $world_id;
+    protected $c_Name;
+    protected $c_HP;
+    protected $c_MP;
+}
--- webroot/www/src/AppBundle/Entity/World.php
+++ webroot/www/src/AppBundle/Entity/World.php
@@ -1,3 +1,12 @@
 <?php
+namespace AppBundle\Entity;
 
-?>
+use Doctrine\ORM\Mapping as ORM;
+
+Class World
+{
+    protected $gm_id;
+    protected $w_Name;
+    protected $w_Description;
+    
+}
