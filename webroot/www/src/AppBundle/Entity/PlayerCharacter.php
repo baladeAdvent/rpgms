cc2dc85f877350bc9499c7a0112dcdef7261553a
David Perez <davidp@thecityof.net>
2016-03-23 15:35:29 -0500

    Regenerating entities, adding .vagrant to ignore list

---
+++ webroot/www/src/AppBundle/Entity/PlayerCharacter.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PlayerCharacter
+ *
+ * @ORM\Table(name="player_character")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlayerCharacterRepository")
+ */
+class PlayerCharacter
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="hp", type="integer")
+     */
+    private $hp;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="mp", type="integer")
+     */
+    private $mp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return PlayerCharacter
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set hp
+     *
+     * @param integer $hp
+     * @return PlayerCharacter
+     */
+    public function setHp($hp)
+    {
+        $this->hp = $hp;
+
+        return $this;
+    }
+
+    /**
+     * Get hp
+     *
+     * @return integer 
+     */
+    public function getHp()
+    {
+        return $this->hp;
+    }
+
+    /**
+     * Set mp
+     *
+     * @param integer $mp
+     * @return PlayerCharacter
+     */
+    public function setMp($mp)
+    {
+        $this->mp = $mp;
+
+        return $this;
+    }
+
+    /**
+     * Get mp
+     *
+     * @return integer 
+     */
+    public function getMp()
+    {
+        return $this->mp;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return PlayerCharacter
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
