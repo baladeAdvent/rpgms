7a33de20b608cb04c503370aa37fb63c34219a83
David Perez <davidp@thecityof.net>
2016-09-21 16:56:51 -0500

    created ajax interval

--- webroot/www/src/RpgmsBundle/Controller/RollViewController.php
+++ webroot/www/src/RpgmsBundle/Controller/RollViewController.php
@@ -9,20 +9,30 @@ use Symfony\Component\HttpFoundation\Response;
 class RollViewController extends Controller
 {
     /**
-     * @Route("/rollviewer", name="Roll Viewer")
+     * @Route("/rollviewer", name="rollviewer")
      */
     public function indexAction()
     {
-        return new Response('RPGMS Roll Viewer');
-        #return $this->render('RpgmsBundle:Default:index.html.twig');
+//        return new Response('RPGMS Roll Viewer');
+
+        return $this->render('RpgmsBundle:Rollviewer:rollview.html.twig', 
+                array(), 
+                null, 
+                null
+        );
     }
-    
-    
+       
     /**
-     *  @Route("/getrolls/{worldName}/{count}", name="Get Rolls")
+     *  @Route("/getrolls/", name="getrolls")
      */
-    public function rollViewAction($worldName, $count)
+    public function viewAction(\Symfony\Component\HttpFoundation\Request $request)
     {        
+        $worldName = 'testWorld';
+        $worldName = $request->query->get('worldName');
+        
+        $count = 5;
+        $count = $request->query->get('count');
+        
         $diceService = $this->get('rpgms.dice_service');
         $worldService = $this->get('rpgms.world_service');
         $world = $worldService->getWorldByName($worldName);
@@ -31,12 +41,11 @@ class RollViewController extends Controller
         $rollSets = $world->getRollSets();
         foreach($rollSets as $rollset)
         {
-            ladybug_dump($rollset);
+//            ladybug_dump($rollset);
         }
         
-        
 //        return new Response('JSON response here...');
-        return $this->render('RpgmsBundle:Rollviewer:rollview.html.twig', 
+        return $this->render('RpgmsBundle:Rollviewer:rollstemplate.html.twig', 
                 array(
                     "rollsets" => $rollSets
                 ), 
